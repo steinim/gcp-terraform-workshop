1b7b7d5d173c9609c58aab8548c573a825859f2d
Stein Inge Morisbak <steinim@gmail.com>
2018-01-03 19:48:20 +0100

    removed unnecessary outputs

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,12 +1,6 @@
 output "name" {
   value = "${google_compute_network.network.name}"
 }
-output "management_subnet_name" {
-  value = "${module.management_subnet.name}"
-}
-output "webservers_subnet_names" {
-  value = "${module.management_subnet.name}"
-}
 output "bastion_public_ip" {
   value = "${module.bastion.public_ip}"
 }
