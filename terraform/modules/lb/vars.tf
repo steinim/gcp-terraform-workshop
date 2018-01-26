6cf10eaac7296290cd29dfb595baa6d9138329d5
Stein Inge Morisbak <steinim@gmail.com>
2018-01-26 18:41:15 +0100

    removed loadbalancer accidentally merged to task5

--- terraform/modules/lb/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "name" {}
-variable "project" {}
-variable "region" {}
-variable "count" {}
-#variable "instances" { type = "list" }
-variable "instance_template" {}
-variable "zones" { type = "list" }
