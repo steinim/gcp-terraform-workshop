8f2c2941e39068426e32454f1716a3fcc2d444db
Stein Inge Morisbak <steinim@gmail.com>
2018-01-28 13:19:45 +0100

    enable sqladmin.googleapis.com

--- terraform/modules/project/main.tf
+++ terraform/modules/project/main.tf
@@ -17,7 +17,8 @@ resource "google_project" "project" {
 resource "google_project_services" "project" {
  project = "${google_project.project.project_id}"
  services = [
-   "compute.googleapis.com"
+   "compute.googleapis.com",
+   "sqladmin.googleapis.com"
  ]
 }
 
