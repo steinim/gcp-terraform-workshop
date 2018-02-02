630210b81040bb9beb07de5bd0a304fb800f5182
Stein Inge Morisbak <steinim@gmail.com>
2018-02-02 12:15:51 +0100

    Revert "Merge branch 'master' into task3"
    This reverts commit c8536a8ed3dd45a090978f2946c068e5b27c18a8, reversing
    changes made to 19a5ed3f99129caed3fde5c84769757c40a8a110.

--- terraform/modules/lb/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "name" {}
-variable "project" {}
-variable "region" {}
-variable "count" {}
-variable "instance_template" {}
-variable "zones" { type = "list" }
--- terraform/modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value = "${google_compute_network.network.name}"
-}
-output "bastion_public_ip" {
-  value = "${module.bastion.public_ip}"
-}
-output "gateway_ipv4"  {
-  value = "${google_compute_network.network.gateway_ipv4}"
-}
